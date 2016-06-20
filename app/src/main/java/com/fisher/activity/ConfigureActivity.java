3e02e079f6bce1980bb660daacaeec03934809a1
jiaoyilun <jiaoyilun@163.com>
2016-06-20 16:05:51 +0800

    commit

--- app/src/main/java/com/fisher/activity/ConfigureActivity.java
+++ app/src/main/java/com/fisher/activity/ConfigureActivity.java
@@ -1,16 +1,44 @@
 package com.fisher.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.fisher.R;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 public class ConfigureActivity extends AppCompatActivity {
+    CoordinatorLayout rootLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure);
+
+        final FloatingActionsMenu actionsMenu = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+
+        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+        actionA.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(rootLayout, "Hello. I am Snackbar!", Snackbar.LENGTH_SHORT).setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+
+            }
+        });
+
+
     }
 
 }
